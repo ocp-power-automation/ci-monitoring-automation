25b5cf4b4ba84b82c385570ed6fac47d60dba7b8
KeerthanaAP <akeerthana1995@gmail.com>
2025-02-20 15:29:01 +0530

    Added fix for the issue in  fetching release info for 4.19 releases
    Signed-off-by: KeerthanaAP <akeerthana1995@gmail.com>

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 PROW_VIEW_URL = "https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs"
 JOB_LINK_URL= "https://prow.ci.openshift.org/"
-RELEASE_URL = "https://ppc64le.ocp.releases.ci.openshift.org/releasestream/4-stable-ppc64le/release/"
+STABLE_RELEASE_URL = "https://ppc64le.ocp.releases.ci.openshift.org/releasestream/4-stable-ppc64le/release/"
+DEV_PREVIEW_RELEASE_URL = "https://ppc64le.ocp.releases.ci.openshift.org/releasestream/4-dev-preview-ppc64le/release/"
 HYPERVISOR_CONNECTION_ERROR = "failed to connect to the hypervisor"
